494afa7edc718a361275d5a4b134474341f3c1f7
mouseorcleg <torindubozit@gmail.com>
2022-10-31 15:00:24 +0000

    Created a table

---
+++ tomatoTomato/Database/AppDatabase.swift
@@ -0,0 +1,43 @@
+//
+//  AppDatabase.swift
+//  tomatoTomato
+//
+//  Created by Maria Kharybina on 31/10/2022.
+//
+
+import GRDB
+
+final class AppDatabase {
+    
+    init(_ dbWriter: any DatabaseWriter) throws {
+        self.dbWriter = dbWriter
+        try migrator.migrate(dbWriter)
+    }
+    
+    private let dbWriter: any DatabaseWriter
+    
+    
+    private var migrator: DatabaseMigrator {
+        var migrator = DatabaseMigrator()
+        
+        #if DEBUG
+        
+        migrator.eraseDatabaseOnSchemaChange = true
+        
+        #endif
+        
+        migrator.registerMigration("createTask") { db in
+            
+            try db.create(table: "task") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("title", .text).notNull()
+                t.column("size", .integer).defaults(to: "S")
+                t.column("type", .text).defaults(to: "Mail")
+                
+            }
+        }
+        
+        return migrator
+    }
+}
+
