d7041ab37c6d9907218b64127e5c67daa1e98e8a
mouseorcleg <torindubozit@gmail.com>
2022-10-31 15:08:04 +0000

    Added a few annotations

--- tomatoTomato/Database/AppDatabase.swift
+++ tomatoTomato/Database/AppDatabase.swift
@@ -2,6 +2,8 @@
 //  AppDatabase.swift
 //  tomatoTomato
 //
+//  Hugely inspared by GRDB sample app: https://github.com/groue/GRDB.swift/blob/master/Documentation/DemoApps/GRDBDemoiOS/GRDBDemoiOS/
+//
 //  Created by Maria Kharybina on 31/10/2022.
 //
 
@@ -26,12 +28,13 @@ final class AppDatabase {
         
         #endif
         
+        // Create a table
         migrator.registerMigration("createTask") { db in
             
             try db.create(table: "task") { t in
                 t.autoIncrementedPrimaryKey("id")
                 t.column("title", .text).notNull()
-                t.column("size", .integer).defaults(to: "S")
+                t.column("size", .text).defaults(to: "S")
                 t.column("type", .text).defaults(to: "Mail")
                 
             }
