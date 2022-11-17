7fea8293accbbc697379bbd34dd9c2d6e88de527
mouseorcleg <torindubozit@gmail.com>
2022-11-17 14:14:04 +0000

    added basic functions to NTVC

--- tomatoTomato/NewTaskViewContoller.swift
+++ tomatoTomato/NewTaskViewContoller.swift
@@ -1,6 +1,7 @@
 //
 //  NewTaskViewContoller.swift
 //  tomatoTomato
+//  Thanks Sean Allen for tutorial about app's lifecycle [https://youtu.be/d7ZqxvbiTyg]
 //
 //  Created by Maria Kharybina on 17/11/2022.
 //
@@ -16,7 +17,7 @@ class NewTaskViewController: UIViewController {
     private(set) var task: tomatoTaskDbModel
     let mode: Mode
     
-    @IBOutlet private weak var saveButtonItem: UIBarButtonItem!
+//    @IBOutlet private weak var saveButtonItem: UIBarButtonItem!
 //    @IBOutlet private weak var
     
     init?(_ coder: NSCoder, mode: Mode, task: tomatoTaskDbModel) {
@@ -29,8 +30,24 @@ class NewTaskViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // when view is created in memory / only one time
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    //every time right before view is on the screen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    //every time view appears on the screen, good for animation
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
+    
+    //every time before the view is dissmised, good for saving stuff
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
     }
 }
